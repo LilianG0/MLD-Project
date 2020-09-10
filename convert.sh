3724b24182d17b624c7fd55d390eaaacf271ef94
Lilian Garcia St20AMT <lgarcia@9lpo58.univ-brest.fr>
2020-09-10 14:29:29 +0000

    Final version

--- convert.sh
+++ convert.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
+
+############################################################################
+### This script is used to re-agroup the ARMOR-3D datasets from one file for
+### each week into one file for each full year. So we have, the dataset goes
+### from ARMOR3D_REPv4_20050105_MLD_OptimalDeBoyer_cmemsformat to 
+### ARMOR_year. It also changes the reference point for the longitude and 
+### latitude. This script in special uses the new ARMOR-3D dataset.
+############################################################################
+
+
+
 ### /data0/data/REFERENCE_DATA/OCEAN_REP/ARMOR3D/data/2009
-
-
-###  dataset-armor-3d-rep-weekly_20100310T1200Z_P20190301T0000Z.nc
-###  ARMOR3D_REPv4_20050105_MLD_OptimalDeBoyer_cmemsformat
-
+###  ARMOR3D_REPv4_20050119_MLD_OptimalDeBoyer_cmemsformat.nc
 
 dir_i=/home/lgarcia/Documents/Original
 file=ARMOR3D_REPv4
@@ -12,24 +19,27 @@ dir_o=/home/lgarcia/Documents/New_ARMOR
 
 
 
-#for year in {1979..2018}; do
-#    cdo sellonlatbox,-180,180,-90,90 $dir/$file       $dir/$file2
-#    ncks -d latitude,10.,85. -d longitude,-90.,50. $dir/$file2 $dir/out.nc
-#echo $month
-#done
+for year in {2005..2018}; do
+    cd $dir_i
 
-for year in {2005..2006}; do
+## First loop into one file per year
+    for f in ARMOR3D_REPv4_$year*.nc; do
+        ncks -O -h --mk_rec_dmn time $f $dir_i/$f-2
+        
+    done
 
-#    for f in $dir_i/$ff*.nc; do
-    echo $f
-    ncks -O -h --mk_rec_dmn time $dir_i/ARMOR3D_REPv4_$year* $dir_o/f-$year.nc
-#    done 
+    cd ..
 
-#    ncrcat -h $dir_o/tmp/$f-2.nc $dir_o/tmp/tmp_$year.nc
+## Changing the reference latitude
+ncrcat -h $dir_i/ARMOR3D_REPv4_$year*.nc $dir_o/tmp_$year.nc
+#    
+ncks -O --msa -d longitude,181.,360. -d longitude,0.,180.0 $dir_o/tmp_$year.nc $dir_o/NARMOR_$year.nc
+ncap2 -O -s 'where(longitude > 180) longitude=longitude-360' $dir_o/NARMOR_$year.nc $dir_o/NARMOR_$year.nc
 
-#    cdo sellonlatbox,-180,180,-90,90 $dir_o/tmp/tmp_$year.nc $dir_o/ARGON_$year.nc
-#    ncks -d latitude,10.,85. -d longitude,-90.,50. $dir_o/ARGON_$year.nc $dir_o/ARGON_$year.nc
+    echo $year
+    
+#    rm -r $dir_i/*-2.nc
+#    rm -r $dir_o/tmp_*.nc
 
-#    rm -f $dir_o/tmp
-#    echo $year
 done
+
